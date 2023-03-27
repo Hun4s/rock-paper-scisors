4758c1636958838a70175cd333f46d7314302039
Hun4s <118963566+Hun4s@users.noreply.github.com>
2023-03-27 10:57:06 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,51 +1,72 @@
 const galimi = ['zirkles', 'akmuo', 'lapas'];
-
 function getComputerChoice() {
   return galimi[Math.floor(Math.random()*galimi.length)];
 };
 
-function game() {
+const zirklesbtn = document.querySelector('#zirklesbtn');
+zirklesbtn.addEventListener('click', () => {
+  const playerSelection = 'zirkles';
+  let computerSelection = getComputerChoice();
+  const container = document.querySelector('#rez');
+  const content = document.createElement('div');
+  content.classList.add('rezultatas');
+  playRound(playerSelection, computerSelection);
+  content.textContent = playRound(playerSelection, computerSelection);
+  container.appendChild(content);
+});
 
-  for (i=0; i<=5; i++) {
+const lapasbtn = document.querySelector('#lapasbtn');
+lapasbtn.addEventListener('click', () => {
+  const playerSelection = 'lapas';
+  let computerSelection = getComputerChoice();
+  const container = document.querySelector('#rez');
+  const content = document.createElement('div');
+  content.classList.add('rezultatas');
+  playRound(playerSelection, computerSelection);
+  content.textContent = playRound(playerSelection, computerSelection);
+  container.appendChild(content);
+});
 
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt('rinkis');
+const akmuobtn = document.querySelector('#akmuobtn');
+akmuobtn.addEventListener('click', () => {
+  const playerSelection = 'akmuo';
+  let computerSelection = getComputerChoice();
+  const container = document.querySelector('#rez');
+  const content = document.createElement('div');
+  content.classList.add('rezultatas');
+  playRound(playerSelection, computerSelection);
+  content.textContent = playRound(playerSelection, computerSelection);
+  container.appendChild(content);
+});
 
-    function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+  
+  if (playerSelection.toLowerCase() === computerSelection) {
+    return 'Lygu';
+  };
 
-      if (playerSelection.toLowerCase() === computerSelection) {
-        return 'Lygu';
-      };
+  if (playerSelection.toLowerCase() === 'akmuo' && computerSelection === 'lapas') {
+    return 'Prapisai, lapas nugali akmeni' ;
+  };
 
-      if (playerSelection.toLowerCase() === 'akmuo' && computerSelection === 'labas') {
-        return 'Prapisai, lapas nugali akmeni' ;
-      };
+  if (playerSelection.toLowerCase() === 'akmuo' && computerSelection === 'zirkles') {
+    return 'Laimejai, akmuo nugali zirkles' ;
+  };
 
-      if (playerSelection.toLowerCase() === 'akmuo' && computerSelection === 'zirkles') {
-        return 'Laimejai, akmuo nugali zirkles' ;
-      };
+  if (playerSelection.toLowerCase() === 'zirkles' && computerSelection === 'akmuo') {
+    return 'Prapisai, akmuo nugali zirkles' ;
+  };
 
-      if (playerSelection.toLowerCase() === 'zirkles' && computerSelection === 'akmuo') {
-        return 'Prapisai, akmuo nugali zirkles' ;
-      };
+  if (playerSelection.toLowerCase() === 'zirkles' && computerSelection === 'lapas') {
+    return 'Laimejai, zirkles nugali lapa' ;
+  };
 
-      if (playerSelection.toLowerCase() === 'zirkles' && computerSelection === 'lapas') {
-        return 'Laimejai, zirkles nugali lapa' ;
-      };
+  if (playerSelection.toLowerCase() === 'lapas' && computerSelection === 'zirkles') {
+    return 'Prapisai, zirkles nugali lapa' ;
+  };
 
-      if (playerSelection.toLowerCase() === 'lapas' && computerSelection === 'zirkles') {
-        return 'Prapisai, zirkles nugali lapa' ;
-      };
-
-      if (playerSelection.toLowerCase() === 'lapas' && computerSelection === 'akmuo') {
-        return 'Laimejai, lapas nugali akmeni' ;
-      };
-
-      if (playerSelection.toLowerCase() !== 'lapas' || playerSelection.toLowerCase() !== 'akmuo' || playerSelection.toLowerCase() !== 'zirkles') {
-        return 'Ismok rasyt' ; 
-      };
-
-    }
-    alert(playRound(playerSelection, computerSelection));
+  if (playerSelection.toLowerCase() === 'lapas' && computerSelection === 'akmuo') {
+    return 'Laimejai, lapas nugali akmeni' ;
   };
 };
+
